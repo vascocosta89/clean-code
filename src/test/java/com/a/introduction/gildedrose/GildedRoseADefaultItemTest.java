d94cfe726ef03f46ed9f694a768d47ababcae374
vascocosta89 <vasco.oliveira.costa@bringglobal.com>
2023-01-25 10:03:32 +0000

    adding step 3 solution

--- src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
+++ src/test/java/com/a/introduction/gildedrose/GildedRoseADefaultItemTest.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 public class GildedRoseADefaultItemTest {
 
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final int MAXIMUM_BRIE_QUALITY = 50;
+
     private static final String DEFAULT_ITEM = "DEFAULT_ITEM";
     private static final int DEFAULT_QUALITY = 4;
     private static final int NOT_EXPIRED_SELLIN = 16;
@@ -45,6 +48,30 @@ public class GildedRoseADefaultItemTest {
         Item expected = new Item(DEFAULT_ITEM, EXPIRED_SELLIN - 1,DEFAULT_QUALITY - 2);
         assertItem(expected,app.items[0]);
     }
+
+    @Test
+    public void unexpiredAgedBrie_qualityIncreasesBy1() {
+        GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, DEFAULT_QUALITY);
+        app.updateQuality();
+        Item expected = new Item(AGED_BRIE, NOT_EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 1);
+        assertItem(expected,app.items[0]);
+    }
+    @Test
+    public void expiredAgedBrie_qualityIncreasesBy2() {
+        GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, EXPIRED_SELLIN, DEFAULT_QUALITY);
+        app.updateQuality();
+        Item expected = new Item(AGED_BRIE, EXPIRED_SELLIN - 1, DEFAULT_QUALITY + 2);
+        assertItem(expected,app.items[0]);
+    }
+
+    @Test
+    public void unexpiredAgedBrie_qualityDoesNotGoBeyondMaximum() {
+        GildedRose app = createGildedRoseWithOneItem(AGED_BRIE, NOT_EXPIRED_SELLIN, MAXIMUM_BRIE_QUALITY);
+        app.updateQuality();
+        Item expected = new Item(AGED_BRIE, NOT_EXPIRED_SELLIN - 1, MAXIMUM_BRIE_QUALITY);
+        assertItem(expected,app.items[0]);
+    }
+
     private void assertItem(Item expected, Item actual) {
         assertEquals(expected.name, actual.name);
         assertEquals(expected.sellIn, actual.sellIn);
