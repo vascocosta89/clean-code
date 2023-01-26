d9fa8dbd88713012d3ab7fd1410bedf47e1c915a
vascocosta89 <vasco.oliveira.costa@bringglobal.com>
2023-01-26 10:02:22 +0000

    adding step5 code

--- src/test/java/com/b/simple/design/business/customer/CustomerBOTest.java
+++ src/test/java/com/b/simple/design/business/customer/CustomerBOTest.java
@@ -5,8 +5,11 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import com.b.simple.design.business.exception.DifferentCurrenciesException;
@@ -25,58 +28,69 @@ public class CustomerBOTest {
 	public void testCustomerProductSum_TwoProductsSameCurrencies()
 			throws DifferentCurrenciesException {
 
-		List<Product> products = new ArrayList<Product>();
+		Amount[] amounts = {
+				new AmountImpl(new BigDecimal("5.0"), Currency.EURO),
+				new AmountImpl(new BigDecimal("6.0"), Currency.EURO) };
 
-		products.add(
-				new ProductImpl(100, "Product 15", ProductType.BANK_GUARANTEE,
-						new AmountImpl(new BigDecimal("5.0"), Currency.EURO)));
+		Amount expected =
+				new AmountImpl(new BigDecimal("11.0"), Currency.EURO);
 
-		products.add(
-				new ProductImpl(120, "Product 20", ProductType.BANK_GUARANTEE,
-						new AmountImpl(new BigDecimal("6.0"), Currency.EURO)));
+		List<Product> products = createProductsWithAmounts(amounts);
 
-		Amount temp = customerBO.getCustomerProductsSum(products);
-
-		assertEquals(Currency.EURO, temp.getCurrency());
-		assertEquals(new BigDecimal("11.0"), temp.getValue());
+		assertAmount(expected, customerBO.getCustomerProductsSum(products));
 	}
 
 	@Test
-	public void testCustomerProductSum1() {
+	public void testCustomerProductSum_TwoProductsDifferentCurrencies()
+			throws DifferentCurrenciesException {
 
-		List<Product> products = new ArrayList<Product>();
+		Amount[] amounts = {
+				new AmountImpl(new BigDecimal("5.0"), Currency.EURO),
+				new AmountImpl(new BigDecimal("6.0"), Currency.INDIAN_RUPEE) };
 
-		products.add(new ProductImpl(100, "Product 15",
-				ProductType.BANK_GUARANTEE,
-				new AmountImpl(new BigDecimal("5.0"), Currency.INDIAN_RUPEE)));
+		List<Product> products = createProductsWithAmounts(amounts);
 
-		products.add(
-				new ProductImpl(120, "Product 20", ProductType.BANK_GUARANTEE,
-						new AmountImpl(new BigDecimal("6.0"), Currency.EURO)));
+		Assertions.assertThrows(DifferentCurrenciesException.class, ()->{
+			customerBO.getCustomerProductsSum(products);
+		});
 
-		@SuppressWarnings("unused")
-		Amount temp = null;
-
-		try {
-			temp = customerBO.getCustomerProductsSum(products);
-			fail("DifferentCurrenciesException is expected");
-		} catch (DifferentCurrenciesException e) {
-		}
 	}
 
+
 	@Test
-	public void testCustomerProductSum2() {
+	public void testCustomerProductSum_EmptyProducts() throws DifferentCurrenciesException {
 
-		List<Product> products = new ArrayList<Product>();
+		Amount actual = customerBO.getCustomerProductsSum(
+				new ArrayList<Product>());
 
-		Amount temp = null;
+		Amount expected = new AmountImpl(BigDecimal.ZERO, Currency.EURO);
 
-		try {
-			temp = customerBO.getCustomerProductsSum(products);
-		} catch (DifferentCurrenciesException e) {
-		}
-		assertEquals(Currency.EURO, temp.getCurrency());
-		assertEquals(BigDecimal.ZERO, temp.getValue());
+		assertAmount(expected, actual);
+	}
+
+	private void assertAmount(Amount expected, Amount actual) {
+		assertEquals(expected.getCurrency(), actual.getCurrency());
+		assertEquals(expected.getValue(), actual.getValue());
+	}
+
+	private List<Product> createProductsWithAmounts(Amount[] amounts) {
+
+		return Arrays.stream(amounts)
+				.map(amount ->
+						new ProductImpl(100, "Product 15", ProductType.BANK_GUARANTEE,
+								amount))
+				.collect(Collectors.toList());
+
+//		List<Product> products = new ArrayList<Product>();
+//
+//		for(Amount amount:amounts) {
+//			products.add(
+//					new ProductImpl(100, "Product 15", ProductType.BANK_GUARANTEE,
+//							amount));
+//
+//		}
+//
+//		return products;
 	}
 
 }
